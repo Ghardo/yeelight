bc86bb4d2468223cd0e49bb44da636d1b59f7529
Ghardo <481103+Ghardo@users.noreply.github.com>
2024-10-18 14:34:50 +0200

    cube matrix support added

--- yeelight.go
+++ yeelight.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"math"
 	"math/rand"
 	"net"
 	"strconv"
@@ -12,11 +13,12 @@ import (
 )
 
 type Yeelight struct {
-	YLID 	   int32       		`json:"id"`
-	Address    string      		`json:"address"`
-	Persistent bool        		`json:"persistent",default0:"false"`
-	Conn       net.Conn		 	`json:"-"`
-	Timeout    time.Duration
+	YLID            int32    `json:"id"`
+	Address         string   `json:"address"`
+	Persistent      bool     `json:"persistent",default0:"false"`
+	Conn            net.Conn `json:"-"`
+	ConnectTimeout  time.Duration
+	ResponseTimeout time.Duration
 }
 
 type Command struct {
@@ -32,15 +34,181 @@ type Response struct {
 }
 
 type Options struct {
-	Smooth     int `default0:"200"`
+	Smooth int `default0:"200"`
 }
 
-type RGB struct {
-	Value int
+type FxMode struct {
+	Mode string `json:"mode"`
 }
 
-func (rgb *RGB) ToHex() string {
-	return fmt.Sprintf("%06x", rgb.Value)
+type Color struct {
+	Value int64
+}
+
+type ColorMatrix struct {
+	Colors []Color
+}
+
+type Vector struct {
+	Row    int
+	Column int
+}
+
+func (v *Vector) Index() int {
+	r := v.Column
+	r += (v.Row * 5)
+	return r
+}
+
+func MakeMatrix(hex string, size int) ColorMatrix {
+	colorMatrix := ColorMatrix{}
+	for i := 0; i < size; i++ {
+		colorMatrix.Colors = append(colorMatrix.Colors, MakeColorHEX(hex))
+	}
+
+	return colorMatrix
+}
+
+func MakeSpotMatrix(hex string) ColorMatrix {
+	colorMatrix := MakeMatrix("#000000", 5)
+	colorMatrix.SetHex(Vector{0, 0}, hex)
+	return colorMatrix
+}
+
+func MakeFromHexColors(matrix []string) ColorMatrix {
+	colorMatrix := ColorMatrix{}
+	for _, element := range matrix {
+		colorMatrix.Colors = append(colorMatrix.Colors, MakeColorHEX(element))
+	}
+
+	return colorMatrix
+}
+
+func (matrix *ColorMatrix) ToASCII() string {
+	ascii := ""
+	for _, element := range matrix.Colors {
+		ascii += element.ToASCII()
+	}
+
+	return ascii
+}
+
+func (matrix *ColorMatrix) ReplaceAllHex(h string) {
+	for index, element := range matrix.Colors {
+		element.Hex(h)
+		matrix.Colors[index] = element
+	}
+}
+
+func (matrix *ColorMatrix) ReplaceAllRGB(r int8, g int8, b int8) {
+	for index, element := range matrix.Colors {
+		element.RGB(r, g, b)
+		matrix.Colors[index] = element
+	}
+}
+
+func (matrix *ColorMatrix) SetHex(v Vector, h string) {
+	matrix.Colors[v.Index()].Hex(h)
+}
+
+func (matrix *ColorMatrix) SetColor(v Vector, c Color) {
+	matrix.Colors[v.Index()] = c
+}
+
+func (matrix *ColorMatrix) GetColor(v Vector) Color {
+	return matrix.Colors[v.Index()]
+}
+
+func (matrix *ColorMatrix) SetRGB(v Vector, r int8, g int8, b int8) {
+	matrix.Colors[v.Index()].RGB(r, g, b)
+}
+
+func (matrix *ColorMatrix) Rotate(angle float64) ColorMatrix {
+	return matrix.RotateAt(angle, Vector{2, 2})
+}
+
+func (matrix *ColorMatrix) RotateAt(angle float64, center Vector) ColorMatrix {
+	new_matrix := MakeMatrix("#000000", 25)
+	a := float64(angle * math.Pi / 180.0)
+
+	cx := float64(center.Column)
+	cy := float64(center.Row)
+
+	for y := 0.0; y < 5.0; y++ {
+		for x := 0.0; x < 5.0; x++ {
+			x_f_new := cx + ((x-cx)*math.Cos(a) - (y-cy)*math.Sin(a))
+			y_f_new := cy + ((x-cx)*math.Sin(a) + (y-cy)*math.Cos(a))
+
+			x_new := int(math.Round(math.Abs(x_f_new)))
+			y_new := int(math.Round(math.Abs(y_f_new)))
+
+			v_old := Vector{Column: int(x), Row: int(y)}
+			v_new := Vector{Column: x_new, Row: y_new}
+
+			color := matrix.GetColor(v_old)
+			new_matrix.SetColor(v_new, color)
+		}
+	}
+
+	return new_matrix
+}
+
+func MakeColorRGB(r int8, g int8, b int8) Color {
+	color := Color{}
+	color.RGB(r, g, b)
+	return color
+}
+
+func MakeColorHEX(hex string) Color {
+	color := Color{}
+	color.Hex(hex)
+	return color
+}
+
+func (color *Color) ToHex() string {
+	return fmt.Sprintf("%06x", color.Value)
+}
+
+func (color *Color) Hex(hexv string) (err error) {
+	color.Value, err = strconv.ParseInt(strings.Trim(hexv, " # "), 16, 64)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (color *Color) RGB(r int8, g int8, b int8) {
+	color.Value = int64(b)
+	color.Value += (int64(r) << 16)
+	color.Value += (int64(g) << 8)
+}
+
+func (color *Color) ToRGB() (r byte, g byte, b byte) {
+	r = byte((color.Value & 0xFF0000) >> 16)
+	g = byte((color.Value & 0x00FF00) >> 8)
+	b = byte(color.Value & 0x0000FF)
+	return
+}
+
+func (color *Color) ToASCII() (result string) {
+	ASCII_TABLE := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	total_bytes := color.Value / 64
+	colorValue := color.Value % 64
+
+	var encoded_data []byte
+
+	encoded_data = append(encoded_data, ASCII_TABLE[total_bytes/4096])
+	total_bytes = total_bytes % 4096
+
+	encoded_data = append(encoded_data, ASCII_TABLE[total_bytes/64])
+	total_bytes = total_bytes % 64
+
+	encoded_data = append(encoded_data, ASCII_TABLE[total_bytes])
+	encoded_data = append(encoded_data, ASCII_TABLE[colorValue])
+	result = string(encoded_data)
+	return
 }
 
 func (c *Command) GenerateID() {
@@ -64,11 +232,11 @@ func (r *Response) FromJson(data []byte) error {
 }
 
 func (yl *Yeelight) Connect() (err error) {
-	if yl.Timeout == 0 {
-		yl.Timeout = 3 * time.Second
+	if yl.ConnectTimeout == 0 {
+		yl.ConnectTimeout = 3 * time.Second
 	}
 
-	yl.Conn, err = net.DialTimeout("tcp", yl.Address, yl.Timeout)
+	yl.Conn, err = net.DialTimeout("tcp", yl.Address, yl.ConnectTimeout)
 	if err != nil {
 		return err
 	}
@@ -95,14 +263,34 @@ func (yl *Yeelight) SendCommand(c Command) (r Response, err error) {
 		return r, err
 	}
 
-	responseBuf := bufio.NewReader(yl.Conn)
-	response, err := responseBuf.ReadSlice('\n')
-	if err != nil {
-		return r, err
+	s := make(chan string)
+	e := make(chan error)
+
+	go func() {
+		reader := bufio.NewReader(yl.Conn)
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			e <- err
+		} else {
+			s <- response
+		}
+		close(s)
+		close(e)
+	}()
+
+	if yl.ResponseTimeout == 0 {
+		yl.ResponseTimeout = 500 * time.Millisecond
 	}
 
-	r.FromJson(response)
-	return r, err
+	select {
+	case response := <-s:
+		r.FromJson([]byte(response))
+		return r, nil
+	case err := <-e:
+		return r, err
+	case <-time.After(yl.ResponseTimeout):
+		return r, nil
+	}
 }
 
 func (yl *Yeelight) GetProperties(names []string) (r Response, err error) {
@@ -153,7 +341,7 @@ func (yl *Yeelight) GetHexColor() (h string, err error) {
 
 	value, err := strconv.Atoi(r.Result.([]interface{})[0].(string))
 
-	rgb := RGB{Value: value}
+	rgb := Color{Value: int64(value)}
 	if err != nil {
 		return h, err
 	}
@@ -269,4 +457,54 @@ func (yl *Yeelight) Sleep(s int8) (err error) {
 	}
 
 	return nil
+}
+
+func (yl *Yeelight) SetMatrix(matrix []ColorMatrix) (err error) {
+	ascii := ""
+
+	for _, element := range matrix {
+		ascii += element.ToASCII()
+	}
+
+	err = yl.SetASCII(ascii)
+
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func (yl *Yeelight) SetASCII(ascii string) (err error) {
+
+	c := Command{
+		Method: "update_leds",
+		Params: []interface{}{ascii},
+	}
+
+	_, err = yl.SendCommand(c)
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func (yl *Yeelight) SetDirectMode() (err error) {
+	mode := FxMode{Mode: "direct"}
+	c := Command{
+		Method: "activate_fx_mode",
+		Params: []interface{}{mode},
+	}
+
+	_, err = yl.SendCommand(c)
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func (yl *Yeelight) Disconnect() {
+	yl.Conn.Close()
 }
